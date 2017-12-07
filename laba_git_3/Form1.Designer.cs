b8a9010895a968be14224d9a5e104a0e38198911
OdiumCraft <OdiumCraft@yandex.ru>
2017-12-07 23:56:06 +0300

    annuitetny calculator

---
+++ laba_git_3/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace laba_git_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.calculat = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.sum = new System.Windows.Forms.TextBox();
+            this.period = new System.Windows.Forms.TextBox();
+            this.percent = new System.Windows.Forms.TextBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // calculat
+            // 
+            this.calculat.Location = new System.Drawing.Point(120, 350);
+            this.calculat.Name = "calculat";
+            this.calculat.Size = new System.Drawing.Size(88, 27);
+            this.calculat.TabIndex = 4;
+            this.calculat.Text = "Расчёт";
+            this.calculat.UseVisualStyleBackColor = true;
+            this.calculat.Click += new System.EventHandler(this.calculat_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Сумма кредита:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Период: (месяцы)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Проценты:";
+            // 
+            // sum
+            // 
+            this.sum.Location = new System.Drawing.Point(191, 97);
+            this.sum.Name = "sum";
+            this.sum.Size = new System.Drawing.Size(100, 20);
+            this.sum.TabIndex = 1;
+            // 
+            // period
+            // 
+            this.period.Location = new System.Drawing.Point(191, 134);
+            this.period.Name = "period";
+            this.period.Size = new System.Drawing.Size(100, 20);
+            this.period.TabIndex = 2;
+            // 
+            // percent
+            // 
+            this.percent.Location = new System.Drawing.Point(191, 170);
+            this.percent.Name = "percent";
+            this.percent.Size = new System.Drawing.Size(100, 20);
+            this.percent.TabIndex = 3;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(44, 232);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Doughnut;
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(247, 109);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(95, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(140, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Аннуитетный калькулятор";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(284, 361);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(44, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Excel";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(41, 207);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(122, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Ежемесячный платеж:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(202, 207);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 392);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.percent);
+            this.Controls.Add(this.period);
+            this.Controls.Add(this.sum);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.calculat);
+            this.MaximumSize = new System.Drawing.Size(350, 430);
+            this.MinimumSize = new System.Drawing.Size(350, 430);
+            this.Name = "Form1";
+            this.Tag = "";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button calculat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox sum;
+        private System.Windows.Forms.TextBox period;
+        private System.Windows.Forms.TextBox percent;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ laba_git_3/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace laba_git_3
+{
+    public partial class Form1 : Form
+    {
+        List<Shedule> collection = new List<Shedule>();
+        public class Shedule
+        {
+            public double _summ { get; set; }
+            public double _persent { get; set; }
+            public int _period { get; set; }
+
+            public double _monthlyPay() => this._summ * (this._persent / 100 / 12 + ((this._persent/ 100 / 12) / (Math.Pow((1 + this._persent / 100 / 12), this._period) - 1)));
+            public double _getPersent()
+            {
+                double allPersents = 0;
+                double mounthIPay;
+                double tmpSumm = this._summ;
+                for(int i = 0;i<_period;i++)
+                {
+                    mounthIPay = tmpSumm * this._persent / 100 / 12;
+
+                    allPersents += mounthIPay;
+
+                    tmpSumm -= _monthlyPay() - mounthIPay;
+                }
+                return allPersents;
+            }
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+   
+        }
+
+        private void calculat_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+
+            var obj = new Shedule
+            {
+                _summ = Convert.ToDouble(sum.Text),
+                _persent = Convert.ToDouble(percent.Text),
+                _period = Convert.ToInt32(period.Text)
+            };
+            collection.Add(obj);
+
+            MessageBox.Show("Объект добавлен в коллекцию!");
+
+            string[] seriesArray = { "Сумма", "проценты" };
+            int[] pointsArray = {432,112303};
+
+            // Add series.
+            label6.Text = $"{Math.Round(obj._monthlyPay(), 2)} Р";
+            chart1.Series[0].Points.AddXY("Cумма", obj._summ);
+            chart1.Series[0].Points.AddXY("Проценты", obj._getPersent());
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(collection.Count == 0)
+            {
+                MessageBox.Show("Коллекция пуста");
+                return;
+            }
+
+            var excelApp = new Excel.Application();
+
+            excelApp.Visible = true;
+
+            excelApp.Workbooks.Add();
+
+            Excel._Worksheet workSheet = excelApp.ActiveSheet;
+
+            workSheet.Cells[1, 1] = "Название кредита";
+            workSheet.Cells[1, 2] = "Сумма кредита";
+            workSheet.Cells[1, 3] = "Срок(мес.)";
+            workSheet.Cells[1, 4] = "Процентная ставка";
+            workSheet.Cells[1, 5] = "Сумма ануитетного платежа";
+            int i = 2;
+            foreach(var a in collection)
+            {
+                workSheet.Cells[i, 1] = $"Кредит №{i-1}";
+                workSheet.Cells[i, 2] = $"{a._summ} Р";
+                workSheet.Cells[i, 3] = a._period;
+                workSheet.Cells[i, 4] = $"{ a._persent}%";
+                workSheet.Cells[i, 5] = $"{ Math.Round(a._monthlyPay(),2) } Р";
+                i++;
+            }
+        }
+    }
+}
